361e10fda34b79458f3478350b122075de1d2314
Liam Westby <liamwestby@icloud.com>
2020-02-26 13:17:04 -0600

    Use cat emoji with cat
    Previous emoji for cat is 🐺 https://emojipedia.org/wolf/. Pretty sure that's not a cat. Uses 🐱 https://emojipedia.org/cat-face/ instead

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Cat 🐺")
+option_a = os.getenv('OPTION_A', u"Cat 🐱")
 option_b = os.getenv('OPTION_B', u"Dog 🐶")
 hostname = socket.gethostname()
 
